9a7cf3b273baf0cc1dc2d17aa3ba123fcf467155
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-30 06:37:34 +1300

    latest

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,25 +7,27 @@ namespace Hangman
 {
     class Program
     {
+        bool _playGame = true;
+
         static void Main(string[] args)
         {
-            var playGame = true;
-            while (playGame)
-                {
-            
-            WriteLine("Would you like to play a game of Hangman? y/n");
-
-            var input = ReadLine();
-
-            var validatedInput = Regex.IsMatch(input, "[a-zA-Z]");
-
-            if (validatedInput && input.ToLower() == "y")
+            var Program = new Program();
+            while (Program._playGame)
             {
-                WriteLine("Genrating a word... please wait");
 
-                var wordList = new List<string>(){ "Abacus", "Triangle", "Car", "Airline" };
+                WriteLine("Would you like to play a game of Hangman? y/n");
 
-                var failurePhrases = new List<string>(){
+                var input = ReadLine();
+
+                var validatedInput = Regex.IsMatch(input, "[a-zA-Z]");
+
+                if (validatedInput && input.ToLower() == "y")
+                {
+                    WriteLine("Genrating a word... please wait");
+
+                    var wordList = new List<string>() { "Abacus", "Triangle", "Car", "Airline" };
+
+                    var failurePhrases = new List<string>(){
                     "Draw and upside-down L This is the post the man hangs from.",
                     "Draw a small circle for the head underneath the horizontal line of the L.",
                     "Draw a line down from the bottom of the head for the body.",
@@ -36,62 +38,85 @@ namespace Hangman
                     "Connect the head to the post with a noose. You are dead."
                 };
 
-                var currentStatus = new List<string>();
+                    var currentStatus = new List<string>();
 
-                var failures = 0;
-                var wordsRemaining = true;
+                    var failures = 0;
+                    var isLetterRemaining = true;
 
-                // Generate a random number to choose a word
-                Random rnd = new Random();
-                int index = rnd.Next(0, wordList.Count);
+                    // Generate a random number to choose a word
+                    Random rnd = new Random();
+                    int index = rnd.Next(0, wordList.Count);
 
-                var answer = wordList[index].ToLower();
+                    var answer = wordList[index].ToLower();
 
-                foreach (char c in answer){
-                    Write("_ ");
-                    currentStatus.Add("_ ");
-                }
-
-                while (failures < 8 && wordsRemaining)
-                {
-                    Write("\n");
-                    WriteLine("Please guess a letter:");
-
-                    var guess = ReadLine();
-
-                    var validatedGuess = Regex.IsMatch(guess, "[a-zA-Z]");
-
-                    if (validatedGuess && answer.Contains(guess.ToLower()))
+                    foreach (char c in answer)
                     {
-                        WriteLine($"You guessed: {guess} correctly!");
-                        while((index=answer.IndexOf(guess, index)) != -1){
-                            currentStatus[index] = guess;
-                                index ++;
-                            }
-                        WriteLine(String.Join("", currentStatus));
+                        Write("_ ");
+                        currentStatus.Add("_ ");
                     }
-                    else {
-                        WriteLine(failurePhrases[failures]);
-                        failures ++;
-                    }
-                }
 
-                if (failures == 8){
-                    WriteLine("Game over, better luck next time. Type q to quit or press any key to continue.");
-                } else {
-                    WriteLine($"Congratulations! You guessed {answer} correctly");
-                }
+                    while (failures < 8 && isLetterRemaining)
+                    {
+                        Write("\n");
+                        WriteLine("Please guess a letter:");
 
-                var exitGame = ReadLine();
-                if (exitGame.Equals('q') || exitGame.Equals('Q'))
+                        var keyEntry = ReadKey();
+                        var guess = keyEntry.KeyChar.ToString();
+                        WriteLine(""); // whitespace for key entry
+
+                        var validatedGuess = Regex.IsMatch(guess, "[a-zA-Z]");
+
+                        if (validatedGuess && answer.Contains(guess.ToLower()))
                         {
-                        playGame = false;
+                            WriteLine($"You guessed: {guess} correctly!");
+                            int idx = 0;
+                            while ((idx = answer.IndexOf(guess, idx)) != -1)
+                            {
+                                currentStatus[idx] = guess;
+                                idx++;
+                            }
+                            //var idx = answer.IndexOf(guess);
+
+                            WriteLine(String.Join("", currentStatus));
+
+                            // have we won?
+                            if (String.Join("", currentStatus) == answer)
+                            {
+                                isLetterRemaining = false;
+                            }
+                        }
+                        else
+                        {
+                            WriteLine(failurePhrases[failures]);
+                            failures++;
+                        }
                     }
 
-            
-                } else {
-                    WriteLine("Closing game..");
-                playGame = false;}
+                    if (failures == 8)
+                    {
+                        WriteLine("Game over, better luck next time. Type q to quit or press any key to continue.");
+                    }
+                    else
+                    {
+                        WriteLine($"Congratulations! You guessed {answer} correctly. Type q to quit or press any key to continue.");
+                        var exitGame = ReadLine();
+                        if (exitGame.ToLower() == 'q'.ToString())
+                        {
+                            Program._playGame = false;
+                        }
+
+                        else
+                        {
+                            Program._playGame = true;
+                        }
+                    }
+                }
+                else
+                {
+                    Program._playGame = false;
+                    WriteLine("Exiting...");
+                }
+
             }
         }
     }
