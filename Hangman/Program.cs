d1aba0cfeee214209a29a5695967a666b885fe1e
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-29 15:53:33 +1300

    Guess logic

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -24,19 +24,35 @@ namespace Hangman
                 Random rnd = new Random();
                 int index = rnd.Next(0, wordList.Count);
 
-                var answer = wordList[index];
+                var answer = wordList[index].ToLower();
 
-                Console.WriteLine("")
+                foreach (char c in answer){
+                    Console.Write("_ ");
+                }
+
+                Console.Write("\n");
+                Console.WriteLine("Please guess a letter:");
+                var guess = Console.ReadLine();
+
+                var validatedGuess = Regex.IsMatch(guess, "[a-zA-Z]");
+
+                if (validatedGuess && answer.Contains(guess.ToLower()))
+                {
+                    Console.WriteLine($"you guessed {guess} correctly");
+                }
+                else {
+                    Console.WriteLine("Incorrect... A post has been erected in the shape of an upside down L.");
+                }
 
 
-                    First wrong answer: Draw and upside-down "L." This is the post the man hangs from.
+                 /*   First wrong answer: Draw and upside-down "L." This is the post the man hangs from.
 Second: Draw a small circle for the "head" underneath the horizontal line of the "L."
 Third: Draw a line down from the bottom of the head for the "body."
 Fourth: Draw one arm out from the middle of his body for the "arm."
 Fifth: Draw the other arm.
 Sixth: Draw one diagonal line from the bottom of the body for the first "leg."
 Seventh: Draw the other leg.
-Eighth: Connect the head to the post with a "noose." Once you draw the noose the players have lost the game.
+Eighth: Connect the head to the post with a "noose." Once you draw the noose the players have lost the game. */
             }
         }
     }
