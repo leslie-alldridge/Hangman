ac2281cb88a4b671ab95caee5720f9c5bda2e36d
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-29 15:46:13 +1300

    start of game

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Hangman
 {
@@ -6,7 +8,36 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Would you like to play a game of Hangman? y/n");
+
+            var input = Console.ReadLine();
+
+            var validatedInput = Regex.IsMatch(input, "[a-zA-Z]");
+
+            if (validatedInput && input.ToLower() == "y")
+            {
+                Console.WriteLine("Genrating a word... please wait");
+
+                var wordList = new List<string>(){ "Abacus", "Triangle", "Car", "Airline" };
+
+                // Generate a random number to choose a word
+                Random rnd = new Random();
+                int index = rnd.Next(0, wordList.Count);
+
+                var answer = wordList[index];
+
+                Console.WriteLine("")
+
+
+                    First wrong answer: Draw and upside-down "L." This is the post the man hangs from.
+Second: Draw a small circle for the "head" underneath the horizontal line of the "L."
+Third: Draw a line down from the bottom of the head for the "body."
+Fourth: Draw one arm out from the middle of his body for the "arm."
+Fifth: Draw the other arm.
+Sixth: Draw one diagonal line from the bottom of the body for the first "leg."
+Seventh: Draw the other leg.
+Eighth: Connect the head to the post with a "noose." Once you draw the noose the players have lost the game.
+            }
         }
     }
 }
