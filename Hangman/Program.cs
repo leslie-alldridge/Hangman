8118f2fa1aba7114233d0665d64eb738a290c0cc
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-29 22:51:01 +1300

    working on index string checking

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
@@ -8,17 +9,37 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Would you like to play a game of Hangman? y/n");
+            var playGame = true;
+            while (playGame)
+                {
+            
+            WriteLine("Would you like to play a game of Hangman? y/n");
 
-            var input = Console.ReadLine();
+            var input = ReadLine();
 
             var validatedInput = Regex.IsMatch(input, "[a-zA-Z]");
 
             if (validatedInput && input.ToLower() == "y")
             {
-                Console.WriteLine("Genrating a word... please wait");
+                WriteLine("Genrating a word... please wait");
 
                 var wordList = new List<string>(){ "Abacus", "Triangle", "Car", "Airline" };
+
+                var failurePhrases = new List<string>(){
+                    "Draw and upside-down L This is the post the man hangs from.",
+                    "Draw a small circle for the head underneath the horizontal line of the L.",
+                    "Draw a line down from the bottom of the head for the body.",
+                    "Draw one arm out from the middle of his body for the arm.",
+                    "Draw the other arm",
+                    "Draw one diagonal line from the bottom of the body for the first leg.",
+                    "Draw the other leg.",
+                    "Connect the head to the post with a noose. You are dead."
+                };
+
+                var currentStatus = new List<string>();
+
+                var failures = 0;
+                var wordsRemaining = true;
 
                 // Generate a random number to choose a word
                 Random rnd = new Random();
@@ -27,32 +48,50 @@ namespace Hangman
                 var answer = wordList[index].ToLower();
 
                 foreach (char c in answer){
-                    Console.Write("_ ");
+                    Write("_ ");
+                    currentStatus.Add("_ ");
                 }
 
-                Console.Write("\n");
-                Console.WriteLine("Please guess a letter:");
-                var guess = Console.ReadLine();
-
-                var validatedGuess = Regex.IsMatch(guess, "[a-zA-Z]");
-
-                if (validatedGuess && answer.Contains(guess.ToLower()))
+                while (failures < 8 && wordsRemaining)
                 {
-                    Console.WriteLine($"you guessed {guess} correctly");
-                }
-                else {
-                    Console.WriteLine("Incorrect... A post has been erected in the shape of an upside down L.");
+                    Write("\n");
+                    WriteLine("Please guess a letter:");
+
+                    var guess = ReadLine();
+
+                    var validatedGuess = Regex.IsMatch(guess, "[a-zA-Z]");
+
+                    if (validatedGuess && answer.Contains(guess.ToLower()))
+                    {
+                        WriteLine($"You guessed: {guess} correctly!");
+                        while((index=answer.IndexOf(guess, index)) != -1){
+                            currentStatus[index] = guess;
+                                index ++;
+                            }
+                        WriteLine(String.Join("", currentStatus));
+                    }
+                    else {
+                        WriteLine(failurePhrases[failures]);
+                        failures ++;
+                    }
                 }
 
+                if (failures == 8){
+                    WriteLine("Game over, better luck next time. Type q to quit or press any key to continue.");
+                } else {
+                    WriteLine($"Congratulations! You guessed {answer} correctly");
+                }
 
-                 /*   First wrong answer: Draw and upside-down "L." This is the post the man hangs from.
-Second: Draw a small circle for the "head" underneath the horizontal line of the "L."
-Third: Draw a line down from the bottom of the head for the "body."
-Fourth: Draw one arm out from the middle of his body for the "arm."
-Fifth: Draw the other arm.
-Sixth: Draw one diagonal line from the bottom of the body for the first "leg."
-Seventh: Draw the other leg.
-Eighth: Connect the head to the post with a "noose." Once you draw the noose the players have lost the game. */
+                var exitGame = ReadLine();
+                if (exitGame.Equals('q') || exitGame.Equals('Q'))
+                        {
+                        playGame = false;
+                    }
+
+            
+                } else {
+                    WriteLine("Closing game..");
+                playGame = false;}
             }
         }
     }
