cebfb997fea3a24a158e6f06e001b64c3acd8728
leslie-alldridge <leslie.alldridge@gmail.com>
2020-12-15 21:03:08 +1300

    Create Game class and Interface, extract Game logic

---
+++ Hangman/Game/Game.cs
@@ -0,0 +1,41 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hangman
+{
+    public class Game : IGame
+    {
+        public IEnumerable<string> FailurePhrasesList => new[]
+        {
+            "Draw and upside-down L This is the post the man hangs from.",
+            "Draw a small circle for the head underneath the horizontal line of the L.",
+            "Draw a line down from the bottom of the head for the body.",
+            "Draw one arm out from the middle of his body for the arm.",
+            "Draw the other arm",
+            "Draw one diagonal line from the bottom of the body for the first leg.",
+            "Draw the other leg.",
+            "Connect the head to the post with a noose. You are dead."
+        };
+
+        public IEnumerable<string> WordList => new[]
+            {"Abacus", "Triangle", "Car", "Airline", "Tree", "Population"};
+
+        public int Guesses
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+                Guesses = value;
+            }
+        }
+
+        public int IncorrectGuesses { get; set; }
+
+        public int RandomNumber => new Random().Next(0, WordList.Count());
+    }
+}
---
+++ Hangman/Game/IGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    public interface IGame
+    {
+        IEnumerable<string> FailurePhrasesList { get; }
+        IEnumerable<string> WordList { get; }
+        int Guesses { get; set; }
+        int IncorrectGuesses { get; set; }
+        int RandomNumber { get; }
+    }
+}
