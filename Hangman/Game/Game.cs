872d36c27fcfd40c61de5e5377724bbcc9d652f8
leslie-alldridge <leslie.alldridge@gmail.com>
2020-12-15 21:26:05 +1300

    Move FailurePhrases and WordList to constructor

--- Hangman/Game/Game.cs
+++ Hangman/Game/Game.cs
@@ -4,23 +4,10 @@ using System.Linq;
 
 namespace Hangman
 {
-    public class Game : IGame
+    class Game : IGame
     {
-        public IEnumerable<string> FailurePhrasesList => new[]
-        {
-            "Draw and upside-down L This is the post the man hangs from.",
-            "Draw a small circle for the head underneath the horizontal line of the L.",
-            "Draw a line down from the bottom of the head for the body.",
-            "Draw one arm out from the middle of his body for the arm.",
-            "Draw the other arm",
-            "Draw one diagonal line from the bottom of the body for the first leg.",
-            "Draw the other leg.",
-            "Connect the head to the post with a noose. You are dead."
-        };
-
-        public IEnumerable<string> WordList => new[]
-            {"Abacus", "Triangle", "Car", "Airline", "Tree", "Population"};
-
+        public IEnumerable<string> FailurePhrasesList { get; set; }
+        public IEnumerable<string> WordList { get; set; }
         public int Guesses
         {
             get
@@ -32,9 +19,23 @@ namespace Hangman
                 Guesses = value;
             }
         }
-
         public int IncorrectGuesses { get; set; }
-
         public int RandomNumber => new Random().Next(0, WordList.Count());
+        public Game()
+        {
+            FailurePhrasesList = new[]
+            {
+                "Draw and upside-down L This is the post the man hangs from.",
+                "Draw a small circle for the head underneath the horizontal line of the L.",
+                "Draw a line down from the bottom of the head for the body.",
+                "Draw one arm out from the middle of his body for the arm.",
+                "Draw the other arm",
+                "Draw one diagonal line from the bottom of the body for the first leg.",
+                "Draw the other leg.",
+                "Connect the head to the post with a noose. You are dead."
+            };
+
+            WordList = new[] { "Abacus", "Triangle", "Car", "Airline", "Tree", "Population" };
+        }
     }
 }
--- Hangman/Game/IGame.cs
+++ Hangman/Game/IGame.cs
@@ -4,8 +4,8 @@ namespace Hangman
 {
     public interface IGame
     {
-        IEnumerable<string> FailurePhrasesList { get; }
-        IEnumerable<string> WordList { get; }
+        IEnumerable<string> FailurePhrasesList { get; set; }
+        IEnumerable<string> WordList { get; set; }
         int Guesses { get; set; }
         int IncorrectGuesses { get; set; }
         int RandomNumber { get; }
